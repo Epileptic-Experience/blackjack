7c9376cc47147e73ed36f5af0b0eb6883605d744
unknown <joaquincerruti54@gmail.com>
2021-10-18 13:13:08 -0300

    hardcoded cards

---
+++ index.js
@@ -0,0 +1,16 @@
+let firstcard = 11
+
+let seconcard = 10
+
+let sum = firstcard + seconcard 
+
+
+console.log(sum)
+
+if (sum < 21) {
+    console.log("want a new card?")
+} else if (sum === 21){
+    console.log("you¡¡¡won!!!")
+} else if (sum > 21) {
+    console.log ("you lost")
+}
