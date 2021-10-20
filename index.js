7d91b48cda370ee021311bbe16b85221a4b89610
unknown <joaquincerruti54@gmail.com>
2021-10-19 22:21:27 -0300

    sarsasarasa

--- index.js
+++ index.js
@@ -1,16 +1,29 @@
 let firstcard = 11
 
-let seconcard = 10
+let seconcard = 11
 
-let sum = firstcard + seconcard 
+let sum = firstcard + seconcard
+
+let hasBlackjack = false
+
+let isAlive = true
+
+let message = ""
 
 
 console.log(sum)
 
-if (sum < 21) {
-    console.log("want a new card?")
-} else if (sum === 21){
-    console.log("you¡¡¡won!!!")
-} else if (sum > 21) {
-    console.log ("you lost")
+if (sum <= 20) {
+    message = "want a new card kid?"
+
 }
+else if (sum === 21) {
+    message = "you got lucky kid"
+    hasBlackjack = true
+}
+else {
+    message = "you're out kid"
+    isAlive = false
+}
+
+console.log (message)
