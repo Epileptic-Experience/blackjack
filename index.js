046f8d9198b6bf38aec249d5c3bb6093afd6ca8d
unknown <joaquincerruti54@gmail.com>
2021-10-24 18:06:05 -0300

    styling basico

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 let firstcard = 11
 
-let seconcard = 11
+let seconcard = 10
 
 let sum = firstcard + seconcard
 
@@ -8,22 +8,29 @@ let hasBlackjack = false
 
 let isAlive = true
 
-let message = ""
+
 
 
 console.log(sum)
 
-if (sum <= 20) {
-    message = "want a new card kid?"
+let message = ""
 
-}
-else if (sum === 21) {
-    message = "you got lucky kid"
-    hasBlackjack = true
-}
-else {
-    message = "you're out kid"
-    isAlive = false
-}
+function startGame() {
+    
 
-console.log (message)
+    if (sum <= 20) {
+        message = "want a new card kid?"
+    
+    }
+    else if (sum === 21) {
+        message = "you got lucky kid"
+        hasBlackjack = true
+    }
+    else {
+        message = "you're out kid"
+        isAlive = false
+    }
+    
+    console.log (message)
+    
+}
